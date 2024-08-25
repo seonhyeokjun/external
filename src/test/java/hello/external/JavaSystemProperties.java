a0f8cd2f42821ba5dd34a2c594f38bbb05e67905
seonhyeokjun <tjsrmatn@naver.com>
2024-08-25 22:48:17 +0900

    외부 설정 - 자바 시스템 속성

---
+++ src/test/java/hello/external/JavaSystemProperties.java
@@ -0,0 +1,25 @@
+package hello.external;
+
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.Properties;
+
+@Slf4j
+public class JavaSystemProperties {
+    public static void main(String[] args) {
+        System.setProperty("hello_key", "hello_value");
+        String helloKey = System.getProperty("hello_key");
+        log.info("hello_key: {}", helloKey);
+
+        Properties properties = System.getProperties();
+        properties.forEach((key, value) -> log.info("property: {}={}", key, value));
+
+        String url = System.getProperty("url");
+        String username = System.getProperty("username");
+        String password = System.getProperty("password");
+
+        log.info("url: {}", url);
+        log.info("username: {}", username);
+        log.info("password: {}", password);
+    }
+}
